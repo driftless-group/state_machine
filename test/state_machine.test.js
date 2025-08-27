e118b4310650f5e0892c0f628d168eef49b7c6b0
Scott Ballantyne <scottballantyne@protonmail.com>
2025-08-27 15:17:10 -0500

    adding a empty test file

---
+++ test/state_machine.test.js
@@ -0,0 +1,22 @@
+const assert   = require('assert');
+const path     = require('path');
+const mongoose = require('mongoose');
+
+
+describe('state_machine', () => {
+
+    after((done) => {
+        done();
+      })
+    })
+
+    xit('i think that there are tests in the original project that I wrote this for', function(done) {
+
+    })
+  })
+})
+
+
+
+
+
